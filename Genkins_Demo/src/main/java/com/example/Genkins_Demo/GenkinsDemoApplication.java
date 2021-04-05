1ac13c3691b1a737c5dca36ec5cedebc1ef29f2f
deepti.surve@transform-hub.com <deepti.surve@transform-hub.com>
2021-04-05 15:30:35 +0530

    first commit

---
+++ Genkins_Demo/src/main/java/com/example/Genkins_Demo/GenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Genkins_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ Genkins_Demo/src/test/java/com/example/Genkins_Demo/GenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Genkins_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
